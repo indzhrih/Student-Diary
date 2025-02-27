545c779aa1fa599d56e47eff43486bdb85fca03b
Alexander Sakovich <sashasakovich06@gmail.com>
2025-02-27 20:56:01 +0300

    Fix optimization problems
    -Optimize CSVExportCommand class

--- commands/csv_export_command.rb
+++ commands/csv_export_command.rb
@@ -5,25 +5,22 @@ module Commands
   class CSVExprotCommand
     class << self
       def export_table(table_name:)
-        data = []
         case table_name
         when :semester
-          Queries::BaseQuery.perform_query(query: 'SELECT * FROM semester').map do |sem|
-            data.push([sem['id'], sem['name'], sem['start_date'], sem['end_date'], sem['active']])
+          data = Queries::BaseQuery.perform_query(query: 'SELECT * FROM semester').map do |sem|
+            [sem['id'], sem['name'], sem['start_date'], sem['end_date'], sem['active']]
           end
         when :discipline
-          Queries::BaseQuery.perform_query(query: 'SELECT * FROM discipline').map do |discipline|
-            data.push([discipline['id'], discipline['name'], discipline['semester_id']])
+          data = Queries::BaseQuery.perform_query(query: 'SELECT * FROM discipline').map do |discipline|
+            [discipline['id'], discipline['name'], discipline['semester_id']]
           end
         when :lab
-          Queries::BaseQuery.perform_query(query: 'SELECT * FROM lab').map do |lab|
-            data.push([lab['id'], lab['name'], lab['deadline'], lab['completed'], lab['mark'], lab['discipline_id']])
+          data = Queries::BaseQuery.perform_query(query: 'SELECT * FROM lab').map do |lab|
+            [lab['id'], lab['name'], lab['deadline'], lab['completed'], lab['mark'], lab['discipline_id']]
           end
         end
 
-        CSV.open("#{table_name}_csv_file.csv", 'w') do |csv|
-          data.each { |row| csv << row }
-        end
+        CSV.open("#{table_name}_csv_file.csv", 'w') { |csv| data.each { |row| csv << row } }
       end
     end
   end
