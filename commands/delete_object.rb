709871d85679ef9a5240616ca8c37976d0dbe6fb
Alexander Sakovich <sashasakovich06@gmail.com>
2025-02-27 19:24:00 +0300

    Fix problems
    -Remove useless code in delete_object.rb

--- commands/delete_object.rb
+++ commands/delete_object.rb
@@ -3,7 +3,7 @@ require_relative '../queries/base_query'
 module Commands
   class DeleteCommand
     def self.perform(object:, object_type:)
-      return Menu.warning(message: 'Пожалуйста введите корректное значение', choice: 4) if object.nil? == true
+      return Menu.warning(message: 'Пожалуйста введите корректное значение', choice: 4) if object.nil?
 
       Queries::BaseQuery.perform_query(
         query: "DELETE FROM #{object_type} WHERE id IN ($1)",
